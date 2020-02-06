4a27266e7a7bd107d7f36758045847b7f9a1846f
Nuno Martins <nuno@alvesmartins.eu>
2020-02-06 20:08:09 +0000

    Fix migrations and models

--- db/migrate/20200204205735_create_ingredients.rb
+++ db/migrate/20200204205735_create_ingredients.rb
@@ -1,6 +1,8 @@
 class CreateIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients do |t|
+      t.string :name, null: false
+      t.references :cocktails
 
       t.timestamps
     end
--- db/migrate/20200204205740_create_doses.rb
+++ db/migrate/20200204205740_create_doses.rb
@@ -1,8 +1,13 @@
 class CreateDoses < ActiveRecord::Migration[6.0]
   def change
     create_table :doses do |t|
+      t.text :description
+      t.references :ingredient, foreign_key: true
+      t.references :cocktail, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :doses, [:ingredient_id, :cocktail_id], unique: true
   end
 end
