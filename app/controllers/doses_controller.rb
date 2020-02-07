b95ae237e5ffbb56343ea6e268e74aaeacfeef21
Nuno Martins <nuno@alvesmartins.eu>
2020-02-07 12:03:09 +0000

    Improve the displaying of the cocktails page and add some navigational buttons

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,7 +1,34 @@
 class DosesController < ApplicationController
+
+  before_action :set_cocktail
+
   def new
+    @dose = Dose.new
   end
 
   def create
+    @dose = @cocktail.doses.new(doses_params)
+
+    # @dose = Dose.new(doses_params)
+    # @dose.cocktail = @cocktail
+
+    if @dose.save
+      # redirect_to cocktail_path(@cocktail)
+      # does the same thing
+      redirect_to @cocktail
+    else
+      render :new
+    end
   end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
 end
